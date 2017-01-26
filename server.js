c34acba3dc7b734c67867b951b2ddcc01bc83097
Jay <misonam03@gmail.com>
2017-01-26 13:25:11 -0600

    very basic node server

--- server.js
+++ server.js
@@ -2,5 +2,17 @@
  * server.js
  * This file defines the server for a
  * simple photo gallery web app.
+ * Author: Jay Ko
  */
- 
+
+"use strict";
+
+var http = require('http');
+var port = 3000;
+var server = http.createServer((req, res) => {
+  res.end("OK"); // this will print out OK on the web browser
+});
+
+server.listen(port,() => {
+  console.log("Listening on Port "+ port);
+});
